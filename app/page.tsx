3a473ac3476efebde2ce64ff81b69ea82364b5bd
5nxtnxtnxt <5nxtnxtnxt@gmail.com>
2024-07-02 11:49:15 +0900

    etc: test

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,8 @@ import { MyData, getDataFromNotion } from "@/utils/notion";
 
 export const dynamic = "force-dynamic";
 
-let data: null | MyData = null;
-console.log(data, "!!!");
 export default async function Home() {
-  console.log(data);
-  if (!data) {
-    data = await getDataFromNotion();
-  }
+  const data = await getDataFromNotion();
   return (
     <main className="">
       <NavigationBar data={data} />
